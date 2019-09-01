8ced61ff2b3022fa64cb8863a910d2b5689b5358
Ryan Styer <ryan_styer@yahoo.com>
2019-09-01 14:22:20 -0400

    Fixed output for totalAdult and totalChild to 2 decimal places

--- Zoo Information System/src/ticketing.java
+++ Zoo Information System/src/ticketing.java
@@ -1,11 +1,16 @@
 //Class calculating and displaying ticket sales for accounting
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class ticketing {
 	
+	private static DecimalFormat df = new DecimalFormat("0.00");
+	
 	//Declaring variables to pass input for ticket numbers and total money sold for children and adults
 	private int ticketNumA, ticketNumC;
-	private double totalAdult, totalChild;
+	private double totalAdult; 
+	private double totalChild;
+	 
 	
 	//Initiating scanner to scan input into variables
 	Scanner s = new Scanner(System.in);
@@ -24,11 +29,12 @@ void getTicketData() {
 //Function displaying calculated totals to be sent to the database class
 void displayTicketData() {
 	
+
 	System.out.println("Test for Ticketing Database");
 	System.out.println("___________________________");
 	System.out.println("Adult tickets sold: " + ticketNumA);
 	System.out.println("Child tickets sold: " + ticketNumC);
-	System.out.println("Total money from adult tickets sold: " + totalAdult);
-	System.out.println("Total money from child tickets sold: " + totalChild);
+	System.out.println("Total money from adult tickets sold: " + df.format(totalAdult));
+	System.out.println("Total money from child tickets sold: " + df.format(totalChild));
 }
 }
